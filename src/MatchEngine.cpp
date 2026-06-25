eebc5d71cfc19430eb7650ced958926098e21bb9
saranathrajaram11 <saranathrajaram11@gmail.com>
2026-06-25 15:44:56 +0530

    Update matching engine logic

--- src/MatchEngine.cpp
+++ src/MatchEngine.cpp
@@ -14,22 +14,47 @@ void  MatchEngine::processorder(Order order)
 
 }; //end of processorder
 
- void MatchEngine::matchbuy(Order buyorder)
+ void MatchEngine::matchbuy(Order incomingbuy)
 {
     if(book.sellorder.empty())
     {
-        book.addorder(buyorder);
+        book.addorder(incomingbuy);
         return;
-    } 
+    } //if sellbook is empty means the incomingbuy should store
     else
     {
-        
+        auto sell_it = book.sellorder.begin();
+        if(incomingbuy.price>=sell_it->first) // comparing of price of incomingbuy with sells.first(pricelevel)
+        {
+            cout<<"trade happended";
+            Order &current=sell_it->second.front(); //access of orderof sell for qty match
+            if(current.qty>incomingbuy.qty){ // if sellqty is bigger then match with incoming.qty
+                current.qty-=incomingbuy.qty;  //reduce the qty of current oder and update on original book
+            return;
+            }
+            else if(current.qty==incomingbuy.qty)
+            {
+                current.qty-=incomingbuy.qty;
+                sell_it->second.pop();
+                return;
+            }
+            else if (current.qty<incomingbuy.qty)
+            {
+               auto remain = incomingbuy.qty- current.qty;
+                book.addorder(incomingbuy);
+            }
+        }
+        else
+        {
+        book.addorder(incomingbuy);
+        return;
+        }
     }
-
+    
 }; //end of matchbuy
 
 
-void MatchEngine::matchsell(Order sellorder)
+void MatchEngine::matchsell(Order incomingsell)
 {
 
 };//end of matchsell
