d07b721d2ee6141e337ad65a83d03414e0c4f358
saranathrajaram11 <saranathrajaram11@gmail.com>
2026-06-27 13:58:21 +0530

    matching engine updation

--- src/MatchEngine.cpp
+++ src/MatchEngine.cpp
@@ -13,40 +13,53 @@ void  MatchEngine::processorder(Order order)
 
  void MatchEngine::matchbuy(Order incomingbuy)
 {
-    if(book.sellorder.empty())
-    {
-        book.addorder(incomingbuy);
-        return;
-    } //if sellbook is empty means the incomingbuy should store
-    else
-    {
-        auto sell_it = book.sellorder.begin();
-        if(incomingbuy.price>=sell_it->first) // comparing of price of incomingbuy with sells.first(pricelevel)
+        while(incomingbuy.qty>0)
         {
+            auto sell_it = book.sellorder.begin();
+         
+            if(incomingbuy.price!=sell_it->first)
+              return;
+
+            if(book.sellorder.empty())
+            {
+                book.addorder(incomingbuy);
+                return;
+            } //if sellbook is empty means the incomingbuy should store
+            
+        
+            if(incomingbuy.price>=sell_it->first) // comparing of price of incomingbuy with sells.first(pricelevel)
+            {
             cout<<"trade happended"<<endl;
             Order &current=sell_it->second.front(); //access of orderof sell for qty match
-            if(current.qty>incomingbuy.qty){ // if sellqty is bigger then match with incoming.qty
-                current.qty-=incomingbuy.qty;  //reduce the qty of current oder and update on original book
-            return;
+        
+              
+                if(current.qty>incomingbuy.qty)
+                { // if sellqty is bigger then match with incoming.qty
+                    current.qty-=incomingbuy.qty;  //reduce the qty of current oder and update on original book
+                    return;
+                }
+              
+                else if(current.qty==incomingbuy.qty)
+                {
+                    current.qty-=incomingbuy.qty;
+                    sell_it->second.pop();
+                    return;
+                }
+              
+                else if (current.qty<incomingbuy.qty)
+                {
+                    auto remain = incomingbuy.qty- current.qty;
+                    incomingbuy.qty=remain;
+                    book.addorder(incomingbuy);
+                }
             }
-            else if(current.qty==incomingbuy.qty)
+            
+            else
             {
-                current.qty-=incomingbuy.qty;
-                sell_it->second.pop();
+                book.addorder(incomingbuy);
                 return;
             }
-            else if (current.qty<incomingbuy.qty)
-            {
-               auto remain = incomingbuy.qty- current.qty;
-                book.addorder(incomingbuy);
-            }
         }
-        else
-        {
-        book.addorder(incomingbuy);
-        return;
-        }
-    }
     
 }; //end of matchbuy
 
